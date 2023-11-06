d9796c598f7648d0d1d6d5c1ff601286b984bc80
MelodyLamphear <melodymassis@gmail.com>
2023-11-05 23:27:29 -0800

    update

--- auction_lamphear.py
+++ auction_lamphear.py
@@ -19,36 +19,50 @@ class Auction:
     def __init__(self, users, bidders):
         self.users = users
         self.bidders = bidders
+        # initialize with all bidders
+        self.active_bidders = list(range(len(bidders)))
+
         # Starts each Bidder with balance of 0 and tracks balance
-        self.balances = {}
-        for bidder_id, _ in enumerate(bidders):
-            self.balances[bidder_id] = 0
+        self.balances = {bidder_id: 0 for bidder_id in self.active_bidders}
 
     def execute_round(self):
         # Step 1: Randomly select a User
         user_id = random.randint(0, len(self.users) - 1)
         chosen_user = self.users[user_id]
+
         # Step 2: Bidders place bids
         bids = {}
-        for bidder_id, bidder in enumerate(self.bidders):
-            bid_amount = bidder.bid(user_id)
+        for bidder_id in self.active_bidders:
+            bid_amount = self.bidders[bidder_id].bid(user_id)
             bids[bidder_id] = bid_amount
+
         # Step 3: Find winning bidder
-        winning_bidder_id = max(bids, key=bids.get)
+        winning_bidders = [bidder_id for bidder_id in self.active_bidders if bids[bidder_id] == max(bids.values())]
+        winning_bidder_id = random.choice(winning_bidders)
         winning_bid = bids[winning_bidder_id]
 
         # Step 4: Determing winning price/find 2nd-highest bid:
         sorted_bids = sorted(bids.values(), reverse=True)
         winning_price = max(sorted_bids[1], winning_bid)
+
         # Step 5: Run show_ad method from User class
         clicked = chosen_user.show_ad()
 
-        # Step 6: Notify bidders of results
-        for bidder_id, bidder in enumerate(self.bidders):
+        # Step 6: Notify top bidder of full results
+        for bidder_id in self.active_bidders:
             won = (bidder_id == winning_bidder_id)
-            bidder_notify = bidder.notify(won, winning_price, clicked)
 
-        # Step 7: Update balance
+            # Notify winner. Rest of bidders should get only the price
+            if won:
+                bidder_notify = self.bidders[bidder_id].notify(won, winning_price, clicked)
+            # else:
+            #     bidder_notify = self.bidders[bidder_id].notify(winning_price)
+
+        # # Step 7: Update balance
         if clicked:
             self.balances[winning_bidder_id] += 1
         self.balances[winning_bidder_id] -= winning_price
+
+        # Prevent negative balances
+        if self.balances[winning_bidder_id] < 0:
+            self.balances[winning_bidder_id] = 0
